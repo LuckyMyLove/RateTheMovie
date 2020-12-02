6c243ec969edf176ea9532b725060cfd906406fe
LuckyMyLove <jagiellojedrzej@gmail.com>
2020-12-02 21:49:50 +0100

    Added Actors site

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('actors/', views.actors, name='actors'),
     path('directors/', views.directors, name='directors')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -28,10 +28,16 @@ def home(request):
 def about(request):
     return render(request, 'dashboard/about.html', {'title': 'About Us'})
 
+def actors(request):
+    obj = Actors.objects.all()
+    context = {
+        'data': obj,
+    }
+    return render(request, 'dashboard/actors_directors.html', context)
 
 def directors(request):
     obj = Directors.objects.all()
     context = {
-        'Directors': obj,
+        'data': obj,
     }
-    return render(request, 'dashboard/directors.html', context)
+    return render(request, 'dashboard/actors_directors.html', context)
