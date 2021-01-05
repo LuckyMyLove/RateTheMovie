c8b849984e1fc7018d32a158163ae3036b3e51e1
LuckyMyLove <jagiellojedrzej@gmail.com>
2021-01-05 18:32:29 +0100

    settings.py + requirements.py updates

--- RateTheMovie/settings.py
+++ RateTheMovie/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.DATE_INPUT_FORMATS com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
