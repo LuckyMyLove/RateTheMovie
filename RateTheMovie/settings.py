76b04833012bde3f0218a88bccd98a53ffad120a
LuckyMyLove <jagiellojedrzej@gmail.com>
2021-01-05 19:51:40 +0100

    Small CSS fix for heroku

--- RateTheMovie/settings.py
+++ RateTheMovie/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import mimetypes
 
+mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
